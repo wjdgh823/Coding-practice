6668a491554f8216129610c317d91326b347f756
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-04-30 00:26:44 +0900

    백준 for 문제 풀이

---
+++ 백준/src/Quiz_for/for_07.java
@@ -0,0 +1,36 @@
+package Quiz_for;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class for_07 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		/*
+		for(int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+		    System.out.println("Case #" + i + " : " + (Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())));
+		}
+		br.close();
+		*/
+		for(int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			bw.write("case #" + i + " : ");
+			bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "\n");
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
