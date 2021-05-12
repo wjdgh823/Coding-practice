608ce5578a619ec97d048dd35d75eb2b4cdf6b12
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-05-12 23:19:51 +0900

    백준 ox퀴즈 8958번 문제풀이

--- 백준/src/Quiz_array/array_05_2.java
+++ 백준/src/Quiz_array/array_05_2.java
@@ -1,10 +1,28 @@
 package Quiz_array;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class array_05_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		double[] val = new double[sc.nextInt()];
+		
+		for (int i = 0; i < val.length; i++) {
+			val[i] = sc.nextDouble();
+		}
+		
+		double sum = 0;
+		Arrays.sort(val); // 숫자 크기대로 나열한다.
+		
+		for(int i = 0; i < val.length; i++) {
+			sum += ( (val[i] / val[val.length-1]) * 100 ); 
+			// 맨 마지막에 있는 max 값을 가져와서 sum의 값에 더해서 저장한다
+		}
+		System.out.println( sum/val.length );
 	}
 
 }
