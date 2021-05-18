585c50a3291f9fad6d8945f3b1bf84cba30d96d4
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-05-18 23:52:49 +0900

    코드업 문자열 1406 : love

---
+++ 코드업/src/Quiz_문자열/Quiz_문자열_3.java
@@ -0,0 +1,21 @@
+package Quiz_문자열;
+
+import java.util.Scanner;
+
+public class Quiz_문자열_3 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		
+		if (str.equals("love")) {
+			System.out.println("I love you");
+		} else {
+			System.out.println("");
+		}
+		
+	}
+
+}
