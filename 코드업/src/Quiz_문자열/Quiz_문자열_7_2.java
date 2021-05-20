da1015a0bbf4c3f0159dd32036eaf8ef3a903158
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-05-20 23:00:04 +0900

    코드업 문자열 C언어 찾기

---
+++ 코드업/src/Quiz_문자열/Quiz_문자열_7_2.java
@@ -0,0 +1,26 @@
+package Quiz_문자열;
+
+import java.util.Scanner;
+
+public class Quiz_문자열_7_2 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+    	
+    	String s = sc.nextLine();
+    	int cnt1 = 0, cnt2 = 0;
+    	
+    	for(int i = 0; i < s.length(); i++) {
+    		if(s.charAt(i)=='c' || s.charAt(i)=='C') {
+    			cnt1++;
+    			if(i < s.length() - 1 && (s.charAt(i+1)=='c' || s.charAt(i+1)=='C'))
+    				cnt2++;
+    		}
+    	}
+    	
+    	System.out.printf("%d\n%d", cnt1, cnt2);
+		
+	}
+
+}
