87081e2af7e89d7423735ba633d389ea89cdec9e
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-05-27 22:59:33 +0900

    코드업 문자열 순환문자열, 3의배수 판별 알고리즘 문제 풀이

---
+++ 코드업/src/Quiz_문자열/Quiz_문자열_13.java
@@ -0,0 +1,22 @@
+package Quiz_문자열;
+
+import java.util.Scanner;
+
+public class Quiz_문자열_13 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		if (num % 3 == 0) {
+			System.out.println("1");
+		} else {
+			System.out.println("0");
+		}
+		
+		
+	}
+
+}
