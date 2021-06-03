b101ed8c2f0df2ae31eadf620bf893478ab0fb27
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-06-03 22:25:24 +0900

    코드업 데이터정렬

---
+++ 코드업/src/Quiz_정렬/Quiz_데이터정렬large.java
@@ -0,0 +1,24 @@
+package Quiz_정렬;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Quiz_데이터정렬large {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		int[] arr = new int[num];
+		
+		for (int i = 0; i < num; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		A
+		
+	}
+
+}
