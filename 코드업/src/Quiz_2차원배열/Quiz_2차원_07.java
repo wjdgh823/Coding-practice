bfdfc20f2ec287ad8a59d9d28e1c96c3fc1593e8
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-06-11 00:09:41 +0900

    코드업  - 2차원배열 1-4

---
+++ 코드업/src/Quiz_2차원배열/Quiz_2차원_07.java
@@ -0,0 +1,30 @@
+package Quiz_2차원배열;
+
+import java.util.Scanner;
+
+public class Quiz_2차원_07 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		int[][]arr = new int[100][100];
+		int n = 1;
+		
+		for (int i = 1; i <= num; i++) {
+			for (int j =num; j >= 1; j--) {
+				arr[j][i] = n++; 
+			}
+		}
+		
+		for (int i = 1; i <= num; i++) {
+			for (int j = 1; j <= num; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
