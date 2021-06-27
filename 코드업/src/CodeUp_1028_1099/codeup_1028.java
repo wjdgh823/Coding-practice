bd8fc3c5a8ea72ae00a21443aee7fc6049a4e9fc
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-06-27 15:05:39 +0900

    codeup 1027 ~ 1029

---
+++ 코드업/src/CodeUp_1028_1099/codeup_1028.java
@@ -0,0 +1,17 @@
+package CodeUp_1028_1099;
+
+import java.util.Scanner;
+
+public class codeup_1028 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int num =sc.nextInt();
+		
+		System.out.println(num);
+		
+	}
+
+}
---
+++ 코드업/src/CodeUp_1028_1099/codeup_1029.java
@@ -0,0 +1,17 @@
+package CodeUp_1028_1099;
+
+import java.util.Scanner;
+
+public class codeup_1029 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		double val = sc.nextDouble();
+		
+		System.out.println(val);
+		
+	}
+
+}
