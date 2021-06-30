b5897f4dba9a777014272c7d4174270d1997af6b
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-06-30 23:30:07 +0900

    codeup 1063 ~ 1064 (삼항연산)

---
+++ 코드업/src/CodeUp_1028_1099/codeup_1063.java
@@ -0,0 +1,18 @@
+package CodeUp_1028_1099;
+
+import java.util.Scanner;
+
+public class codeup_1063 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		int result = num1 > num2 ? num1 : num2;
+		System.out.println(result);
+	}
+
+}
