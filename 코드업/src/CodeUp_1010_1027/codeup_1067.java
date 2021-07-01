9b4556256d195175fd86dfabe81b816b7914b32b
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-07-01 22:40:04 +0900

    codeup 1065 ~ 1067

---
+++ 코드업/src/CodeUp_1010_1027/codeup_1067.java
@@ -0,0 +1,31 @@
+package CodeUp_1010_1027;
+
+import java.util.Scanner;
+
+public class codeup_1067 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		if (num * -1 > 0) {
+			System.out.println("minus");
+			if (num % 2 == 0) {
+				System.out.println("even");
+			} else {
+				System.out.println("odd");
+			}
+		} else {
+			System.out.println("plus");
+			if (num*-1 % 2 == 0) {
+				System.out.println("even");
+			} else {
+				System.out.println("odd");
+			}
+		}
+		
+	}
+
+}
