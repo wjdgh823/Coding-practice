8278dcdc85ac24ff10a82f916af423a2437d3a5e
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-07-08 23:45:39 +0900

    codeup 1079

---
+++ 코드업/src/CodeUp_1028_1099/codeup_1079.java
@@ -0,0 +1,22 @@
+package CodeUp_1028_1099;
+
+import java.util.Scanner;
+
+public class codeup_1079 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+			
+		String str;
+		while (true) {
+			str = sc.next();
+			System.out.println(str);
+			if (str.equals("q")) {
+				break;
+			}
+		}
+		
+	}
+
+}
