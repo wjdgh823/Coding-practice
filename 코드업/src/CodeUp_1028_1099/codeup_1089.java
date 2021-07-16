b88a1e8dcae76f8d9fee91830ee9fe11de6d6e2f
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-07-17 00:32:27 +0900

    코드업 1089

--- 코드업/src/CodeUp_1028_1099/codeup_1089.java
+++ 코드업/src/CodeUp_1028_1099/codeup_1089.java
@@ -8,26 +8,18 @@ public class codeup_1089 {
 
 		Scanner sc = new Scanner(System.in);
 		
-		int num1 = sc.nextInt();
-		int num2 = sc.nextInt();
-		int num3 = sc.nextInt();
+		String[] numList = sc.nextLine().split(" ");
 		
+		int a = Integer.parseInt(numList[0]);
+		int b = Integer.parseInt(numList[1]);
+		int c = Integer.parseInt(numList[2]);
 		
-		int total = 0;
 		int cnt = 0;
-		while(true) {
-			cnt++;
-			if (cnt == 1) {
-				total += (num1 + num2);
-			} else if (cnt != 1) {
-				total += (total + num2);
-			}
-			
-			if (cnt == num3) {
-				System.out.println(total);
-				return;
-			}
+		while(cnt != c-1) {
+			a += b;
+			cnt += 1;
 		}
+		System.out.println(a);
 		
 	}
 
