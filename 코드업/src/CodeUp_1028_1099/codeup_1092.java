dc89c57a917229071e852c4a0aa37f9162e0629e
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-07-20 23:06:50 +0900

    codeup 1092

---
+++ 코드업/src/CodeUp_1028_1099/codeup_1092.java
@@ -0,0 +1,24 @@
+package CodeUp_1028_1099;
+
+import java.util.Scanner;
+
+public class codeup_1092 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int tum1 = sc.nextInt();
+		int tum2 = sc.nextInt();
+		int tum3 = sc.nextInt();
+		
+		int day = 1;
+		while(tum1 != 0 && tum2 != 0 && tum3 != 0) {
+			if (day % tum1 == 0 && day % tum2 == 0 && day % tum3 == 0) break;
+				day++;
+		}
+		System.out.println(day);
+		
+	}
+
+}
