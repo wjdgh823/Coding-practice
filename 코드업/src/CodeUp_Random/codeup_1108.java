367783439869bd9dbad2f95272a8393c9ac9120e
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-08-01 17:27:24 +0900

    codeup_random_Problem

---
+++ 코드업/src/CodeUp_Random/codeup_1108.java
@@ -0,0 +1,19 @@
+package CodeUp_Random;
+
+public class codeup_1108 {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 20; i++) {
+			System.out.print("hello");
+			if (i == 19) {
+				System.out.print("hello ");
+			}
+		}
+		for (int j = 0; j < 20; j++) {
+			System.out.print("world");
+		}
+		
+	}
+
+}
---
+++ 코드업/src/CodeUp_Random/codeup_1109.java
@@ -0,0 +1,22 @@
+package CodeUp_Random;
+
+import java.util.Scanner;
+
+public class codeup_1109 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String name = sc.next();
+		int age = sc.nextInt();
+		String code = sc.next();
+		Double keyNum = sc.nextDouble();
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(code);
+		System.out.println(keyNum);
+		
+	}
+
+}
