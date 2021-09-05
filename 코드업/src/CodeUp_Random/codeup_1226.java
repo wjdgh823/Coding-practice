a1321ec41625c853fd61973907d17303d05453ce
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-09-05 18:44:21 +0900

    codeup 1226(로또 번호) 알고리즘 수정

--- 코드업/src/CodeUp_Random/codeup_1226.java
+++ 코드업/src/CodeUp_Random/codeup_1226.java
@@ -7,26 +7,31 @@ public class codeup_1226 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int[] arr = new int[7];
-		int correct = 0;
-		int bonus_correct = 0;
-		int n;
 		
-		for (int i = 0; i < 7; i++) {
-			arr[i] = (int)(Math.random()*10);
-			System.out.print(arr[i] + " ");
+		int[] lottoArr = new int[7];
+		int[] myLottoArr = new int[6];
+		
+		for (int i = 0; i < lottoArr.length; i++) {
+			lottoArr[i] = (int)(Math.random()*10);
+			System.out.print(lottoArr[i] + " ");
 		}
+		System.out.print("로또 번호");
 		
+		for (int i = 0; i < myLottoArr.length; i++) {
+			myLottoArr[i] = sc.nextInt();
+			System.out.print(myLottoArr[i] + " ");
+		}
+		System.out.println("내 로또 번호");
 		
-		for (int i = 0; i < 6; i++ ) {
-			n = sc.nextInt();
-			for (int j = 0; j < 7; j++ ) {
-				if (arr[j] == n) {
-					if (j==6) {
-						bonus_correct++;
-					} else {
-						correct++;
-					}
+		int correct = 0;
+		int bonus = 0;
+		
+		for (int i = 0; i < myLottoArr.length; i++) {
+			if (myLottoArr[i] == lottoArr[i]) {
+				correct++;
+			} else {
+				if (lottoArr[6] == myLottoArr[i]) {
+					bonus++;
 				}
 			}
 		}
@@ -37,11 +42,11 @@ public class codeup_1226 {
 			System.out.println("5등");
 		} else if (correct == 4) {
 			System.out.println("4등");
-		} else if (correct == 5&&bonus_correct==0) {
+		} else if (correct == 5&&bonus==0) {
 			System.out.println("3등");
-		} else if (correct == 5&&bonus_correct==1) {
+		} else if (correct == 5&&bonus==1) {
 			System.out.println("2등");
-		}  else if(correct == 6) {
+		} else if (correct == 6) {
 			System.out.println("1등");
 		}
 		
