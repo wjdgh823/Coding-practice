a934eb1f18bfbbb6f04cb0ed4be2ba2362566255
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-09-08 23:07:10 +0900

    codeup 1251 ~ 1254

---
+++ 코드업/src/CodeUp_Random/codeup_1252.java
@@ -0,0 +1,19 @@
+package CodeUp_Random;
+
+import java.util.Scanner;
+
+public class codeup_1252 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.print(i + " ");
+		}
+		
+	}
+
+}
